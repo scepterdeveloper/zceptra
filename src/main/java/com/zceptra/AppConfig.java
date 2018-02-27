f5cbeec4f647c8e045300ed9899a6ab6ce1fe009
Sandeep Joseph <sandeep.josef@gmail.com>
2018-02-27 23:15:48 +0100

    Post category service

--- src/main/java/com/zceptra/AppConfig.java
+++ src/main/java/com/zceptra/AppConfig.java
@@ -1,18 +1,15 @@
 package com.zceptra;
 
-import org.h2.server.web.WebServlet;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
 	
-    @Bean
+ /*   @Bean
     ServletRegistrationBean h2servletRegistration(){
 
         ServletRegistrationBean registrationBean = new ServletRegistrationBean( new WebServlet());
         registrationBean.addUrlMappings("/console/*");
         return registrationBean;
-    }	
+    }	*/
 }
--- src/main/java/com/zceptra/controllers/CategoryController.java
+++ src/main/java/com/zceptra/controllers/CategoryController.java
@@ -2,7 +2,10 @@ package com.zceptra.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.zceptra.entities.Category;
@@ -11,8 +14,6 @@ import com.zceptra.repositories.CategoryRepository;
 @RestController
 public class CategoryController {
 	
-	//test
-
 	@Autowired
 	private CategoryRepository repository;
 	
@@ -28,5 +29,19 @@ public class CategoryController {
 	public Category getCategory(Long id)	{
 		
 		return repository.findOne(id);
-	}	
+	}
+	
+	@CrossOrigin(origins = {"https://zceptra-ui.herokuapp.com", "http://localhost:4200"})
+	@RequestMapping(value="edit-category", method=RequestMethod.POST)
+	public void saveCategory(@RequestBody Category category)	{
+		
+		
+		Category savedCategory = repository.findOne(category.getId());
+		savedCategory.setDescription(category.getDescription());
+		repository.save(savedCategory);
+		
+		System.out.println("POST");
+		System.out.println("Category: " + category.getId() + " | " + category.getName() + " | " + category.getDescription());
+		//repository.save(category);
+	}
 }
