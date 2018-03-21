aa6a75cbfb361f17ea2ac2efabaf2dff555cd908
Sandeep Joseph <sandeep.josef@gmail.com>
2018-03-21 22:22:18 +0100

    transaction type fn

--- src/main/java/com/zceptra/controllers/TransactionTypeController.java
+++ src/main/java/com/zceptra/controllers/TransactionTypeController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,7 +27,7 @@ public class TransactionTypeController {
 	
 	@CrossOrigin(origins = {"https://zceptra-ui.herokuapp.com", "http://localhost:4200"})
 	@RequestMapping(value="get-transaction-type")
-	public TransactionType getTransactionType(Long id)	{
+	public TransactionType getTransactionType(@RequestParam Long id)	{
 		
 		return repository.findOne(id);
 	}	
